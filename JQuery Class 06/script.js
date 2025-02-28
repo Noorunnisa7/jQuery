32c08b0292229158412cda5dae4ab67e672fc514
noorunnisa7 <noorunnisa560@gmail.com>
2025-02-28 15:51:53 +0500

    JQuery Loader

---
+++ JQuery Class 06/script.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+
+    setTimeout(() => {
+        $('.pageLoader').fadeOut()
+    } , 3000)
+})
+
+
+
+
+
+// document.write("hello ")
+
+
+// setTimeout(() => {
+//     alert("Time Is out")
+// }, 8000);
